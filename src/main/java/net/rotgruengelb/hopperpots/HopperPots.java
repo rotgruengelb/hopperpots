4040f986ae70bb7c61f18b63b0ec03f43d53b8ca
Daniel (aka. rotgruengelb) <rotgrungelblab@gmail.com>
2024-07-05 16:51:35 +0200

    Update to 1.21

--- src/main/java/net/rotgruengelb/hopperpots/HopperPots.java
+++ src/main/java/net/rotgruengelb/hopperpots/HopperPots.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
 public class HopperPots implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("hopperpots");
 	public static final GameRules.Key<GameRules.BooleanRule> GAMERULE_HOPPER_SPECIAL_BLOCKS = GameRuleRegistry.register("hopperSpecialBlocks", net.minecraft.world.GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
-	public static final TagKey<Block> BLOCK_TAG_HOPPER_SPECIAL_BLOCKS = TagKey.of(RegistryKeys.BLOCK, new Identifier("c", "hopper_special_blocks"));
+	public static final TagKey<Block> BLOCK_TAG_HOPPER_SPECIAL_BLOCKS = TagKey.of(RegistryKeys.BLOCK, Identifier.of("c", "hopper_special_blocks"));
 
 	@Override
 	public void onInitialize() {
